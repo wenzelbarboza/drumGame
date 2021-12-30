9e2644bfbc77cd003d5d040d229acfc819c3a655
Wenzel <wenzelbarboza@gmail.com>
2021-12-30 16:36:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+let drums = document.querySelectorAll(".drum");
+
+for(let i = 0; i < drums.length; i++){
+    drums[i].addEventListener("click",function (){
+        
+        let innerHTML = this.innerHTML;
+        switch(innerHTML){
+            case "w":
+                let t1 = new Audio("sounds/tom-1.mp3")
+                t1.play();
+                break;
+            
+            case "a":
+                let t2 = new Audio("sounds/tom-2.mp3")
+                t2.play();
+                break;
+            
+            case "s":
+                let t3 = new Audio("sounds/tom-3.mp3")
+                t3.play();
+                break;
+                
+            case "d":
+                let t4 = new Audio("sounds/tom-4.mp3")
+                t4.play();
+                break;
+        
+            case "j":
+                let snare = new Audio("sounds/snare.mp3")
+                snare.play();
+                break;
+
+            case "k":
+                let crash = new Audio("sounds/crash.mp3")
+                crash.play();
+                break;
+
+            case "l":
+                let kickBass = new Audio("sounds/kick-bass.mp3")
+                kickBass.play();
+                break;
+        }
+
+    })
+}
+
+
+
+
+
